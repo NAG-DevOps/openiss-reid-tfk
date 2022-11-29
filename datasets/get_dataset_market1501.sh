3c53f0ef797fc16acab758b5849c36500aa0d1bf
carlos <carlos@encs.concordia.ca>
2022-11-29 17:03:54 -0500

    Commands sequence modified

--- datasets/get_dataset_market1501.sh
+++ datasets/get_dataset_market1501.sh
@@ -1,5 +1,4 @@
 wget https://bitbucket.org/eric_thesis/reid-tfk/downloads/market1501.zip
-mv Market-1501-v15.09.15.zip market1501.zip
 unzip market1501.zip
 rm -rf market1501.zip
 mv Market-1501-v15.09.15 market1501
